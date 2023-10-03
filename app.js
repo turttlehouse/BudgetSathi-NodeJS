ea55d4b3cb7dea5de0568a317f5a333012d63479
turttlehouse <buidandrun464@gmail.com>
2023-10-03 20:57:05 +0545

    For SSR views folder created and dashboard sidebar made,public folder created and configured it to allow for public availability in app.js

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-// using express package from npm for routing to define routes, Templates to use EJS engine which help in generating dynamic HTML content on the server and sending it to the client,
-//For static file handling : we can serve static files easily using express
-//For security : Express provides a foundation for implementing security practices such as input validation,authentication, and authorization.
-const express= require("express") ()
+// const app = require("express") () yo garda express.static use garda error ayo public file ko lagi
+//Importing the Express.js library and initializing an instance of an Express application
+const express= require("express") 
+const app = express()
+
+//setting the port variable to 5000, indicating that the application will listen on port 5000 for incoming network requests.
 const port = 5000;
 
-//set the viewengine to ejs -> setting the view engine to 'ejs' telling Express.js that when we render views or templates (e.g., when using res.render()), it should use the EJS templating engine to process and render those templates. 
-express.set('view engine','ejs');
+//set the viewengine to ejs -> telling Express.js that when we render views or templates (e.g., when using res.render()), it should use the EJS templating engine to process and render those templates. 
+app.set('view engine','ejs');
+
+//This line of code is configuring express.js to serve static files located in the public directory. By default NodeJS ko kunai pani directory public hudaina
+app.use(express.static("public/"))
 
 
 //Get API - defining the route for '/'
-express.get('/',(req,res)=>{
-    res.render("home");
+app.get('/',(req,res)=>{
+    //rendering the home.ejs file inside views folder
+    res.render("Overview");
 })
 
 
@@ -23,8 +29,13 @@ express.get('/',(req,res)=>{
 
 
 
- //Installin nodemon - Automatically restarting our Node.js application whenever changes are detected in our codebase
 // After the server start listening on a specific port for incoming requests
-express.listen(port,(req,res)=>{
+app.listen(port,(req,res)=>{
     console.log("NodeJs Project has started at port no. 5000")
 })
+
+//Packages used
+
+//express - For -> (Routing) to define routes, (Templates) to use EJS engine which help in generating dynamic HTML content on the server and sending it to the client,(For static file handling) : we can serve static files easily using express,(For security) : Express provides a foundation for implementing security practices such as input validation,authentication, and authorization.
+//nodemon - Automatically restarting our Node.js application whenever changes are detected in our codebase
+// ejs - Embedded JavaScript a template engine for JavaScript that allows us to embed dynamic JavaScript code within our HTML or other markup templates. 
